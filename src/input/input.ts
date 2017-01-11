d2337d9d928a7e8793d9f8f5b68d391812054315
Stephen Lautier <stephen.lautier@outlook.com>
2017-01-11 02:15:49 +0100

    input: implemented initial style + implemented disabled, value, handling id automatically instead of a param

--- src/input/input.ts
+++ src/input/input.ts
@@ -1,29 +1,36 @@
 import { LoggerFactory, ILog } from "@ssv/au-core";
 import { customElement, ViewResources, bindable, View, processAttributes } from "aurelia-templating";
 import { autoinject } from "aurelia-dependency-injection";
+import { bindingMode } from "aurelia-binding";
 import { Themable, StyleEngine, processDesignAttributes } from "../ux/index";
 
 @autoinject()
 @customElement("ssv-input")
 @processAttributes(processDesignAttributes)
 export class Input implements Themable {
+	static id = 0;
 
-	@bindable id: string;
+	@bindable({
+		defaultBindingMode: bindingMode.twoWay
+	}) value: string;
 	@bindable label: string;
 	@bindable placeholder: string | null = null;
-	@bindable disabled = false;
+	@bindable disabled: boolean | string = false;
 	@bindable theme: string | null = null;
 
 	view: View;
+	input: HTMLInputElement;
+	controlId: string;
 
 	private logger: ILog;
 
 	constructor(
+		loggerFactory: LoggerFactory,
 		public resources: ViewResources,
 		private styleEngine: StyleEngine,
-		loggerFactory: LoggerFactory
 	) {
 		this.logger = loggerFactory.get("input");
+		this.controlId = `ssv-input-${Input.id++}`;
 	}
 
 	created(_: any, myView: View) {
@@ -36,6 +43,18 @@ export class Input implements Themable {
 		if (this.theme) {
 			this.styleEngine.applyTheme(this, this.theme);
 		}
+		this.disabled = getAttributeFlagAsBoolean(this.disabled);
+		this.input.disabled = this.disabled;
+	}
+
+	attached() {
+		this.logger.debug("attached", "init", { disabled: this.disabled });
+	}
+
+	disabledChanged(newValue: boolean) {
+		if (this.input) {
+			this.input.disabled = !!newValue;
+		}
 	}
 
 	themeChanged(newValue: string | null) {
@@ -43,4 +62,9 @@ export class Input implements Themable {
 		this.styleEngine.applyTheme(this, newValue);
 	}
 
+}
+
+// todo: move somewhere reusable.
+export function getAttributeFlagAsBoolean(value: string | boolean): boolean {
+	return value === true || value === "true" || value === "";
 }
