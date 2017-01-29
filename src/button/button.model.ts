09537163944e24911f92eb8e752d6a4db5dc9caa
Stephen Lautier <stephen.lautier@outlook.com>
2017-01-29 16:41:40 +0100

    button: removed fab for now

--- src/button/button.model.ts
+++ src/button/button.model.ts
@@ -1,8 +1,7 @@
-export type ButtonType = "flat" | "raised" | "fab" | "outline";
+export type ButtonType = "flat" | "raised" | "outline";
 
 export const buttonType = {
 	flat: "flat" as ButtonType,
 	raised: "raised" as ButtonType,
-	fab: "fab" as ButtonType,
 	outline: "outline" as ButtonType,
 };
