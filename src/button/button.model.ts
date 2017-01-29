7178e0a6f190d4d97af38ae8d1902c84585f0a24
Stephen Lautier <stephen.lautier@outlook.com>
2017-01-29 15:41:09 +0100

    button: added outline type

--- src/button/button.model.ts
+++ src/button/button.model.ts
@@ -1,7 +1,8 @@
-export type ButtonType = "flat" | "raised" | "fab";
+export type ButtonType = "flat" | "raised" | "fab" | "outline";
 
 export const buttonType = {
 	flat: "flat" as ButtonType,
 	raised: "raised" as ButtonType,
 	fab: "fab" as ButtonType,
+	outline: "outline" as ButtonType,
 };
