df6fc1b4d8f010000296c8952a9999a195f86fa4
Stephen Lautier <stephen.lautier@outlook.com>
2017-02-12 15:58:13 +0100

    style(all): import order formatting

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 export * from "./core/index";
-export * from "./input/index";
+
 export * from "./badge/index";
 export * from "./button/index";
 export * from "./checkbox/index";
-export * from "./waves/index";
 export * from "./icon/index";
+export * from "./input/index";
+export * from "./waves/index";
 
 export * from "./config";
 export * from "./config.model";
