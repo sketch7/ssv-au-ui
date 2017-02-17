fc612898fd592612d52bf0a4d4dcb7113f9c188d
Stephen Lautier <stephen.lautier@outlook.com>
2017-02-17 01:59:04 +0100

    cleanup simple snackbar

--- src/snackbar/simple-snackbar.element.ts
+++ src/snackbar/simple-snackbar.element.ts
@@ -1,10 +1,7 @@
-import * as _ from "lodash";
 import { DOM } from "aurelia-pal";
 import { customElement, bindable } from "aurelia-templating";
 import { autoinject } from "aurelia-dependency-injection";
 import { LoggerFactory, ILog } from "@ssv/au-core";
-
-import { snackbarConfig, SnackbarConfig } from "./snackbar.config";
 
 const PREFIX = "ssv-simple-snackbar";
 
@@ -14,10 +11,8 @@ export class SimpleSnackbarElement {
 
 	@bindable label: string;
 	@bindable action: string;
-	@bindable color: string;
 
 	private logger: ILog;
-	private config: SnackbarConfig;
 
 	constructor(
 		loggerFactory: LoggerFactory,
@@ -26,24 +21,10 @@ export class SimpleSnackbarElement {
 		this.logger = loggerFactory.get("simpleSnackbarElement");
 	}
 
-	bind() {
-		this.setDefaults();
-
-		if (this.config.color) {
-			this.element.classList.add(`${PREFIX}--${this.config.color.toLowerCase()}`);
-		}
-	}
-
 	onAction() {
 		this.logger.debug("onAction");
 		const event = DOM.createCustomEvent("action", {});
 		this.element.dispatchEvent(event);
-	}
-
-	private setDefaults(): void {
-		this.config = _.defaults<SnackbarConfig>({
-			color: this.color,
-		}, snackbarConfig);
 	}
 
 }
