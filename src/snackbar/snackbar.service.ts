0a85584c6f2e0386b999f5247e7af9d65f402f20
Stephen Lautier <stephen.lautier@outlook.com>
2017-02-24 02:04:37 +0100

    snackbar: added behaviour to service to handle next + auto dimiss

--- src/snackbar/snackbar.service.ts
+++ src/snackbar/snackbar.service.ts
@@ -1,5 +1,6 @@
 import { autoinject } from "aurelia-dependency-injection";
 import { BindingEngine, PropertyObserver } from "aurelia-binding";
+import { ILog, LoggerFactory } from "@ssv/au-core";
 
 import { SnackbarRef, SnackbarOptions } from "./snackbar-ref";
 
@@ -9,25 +10,53 @@ import { SnackbarRef, SnackbarOptions } from "./snackbar-ref";
 @autoinject()
 export class SnackbarService {
 
-	activeItem: SnackbarRef;
+	activeItem: SnackbarRef | null;
 	activeItem$: PropertyObserver;
 
 	private items: SnackbarRef[];
+	private logger: ILog;
 
 	constructor(
+		loggerFactory: LoggerFactory,
 		private bindingEngine: BindingEngine,
 	) {
+		this.logger = loggerFactory.get("snackbarService");
 		this.items = [];
 		this.activeItem$ = this.bindingEngine.propertyObserver(this, "activeItem");
+		this.activeItem$.subscribe(x => {
+			if (!x && this.items.length) {
+				this.handleNext();
+			}
+		});
 	}
 
 	open(message: string, action?: string, options?: SnackbarOptions): SnackbarRef {
 		const item = new SnackbarRef(message, action, options);
+		item.onDismiss(() => {
+			this.activeItem = null;
+		});
 		this.add(item);
-
-		// todo: handle active better
-		this.activeItem = item;
+		if (this.activeItem) {
+			return item;
+		}
+		this.handleNext();
 		return item;
+	}
+
+	private handleNext() {
+		this.logger.debug("handleNext");
+		const next = this.items.shift();
+		if (!next) {
+			return;
+		}
+
+		this.activeItem = next;
+		setTimeout(() => {
+			if (!this.activeItem) {
+				return;
+			}
+			this.activeItem.dismiss();
+		}, 3000); // todo: make timer configurable
 	}
 
 	private add(item: SnackbarRef) {
