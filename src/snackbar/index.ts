328f6f5ca4a306f3c0c6a1f2c5c310d18247ca20
Stephen Lautier <stephen.lautier@outlook.com>
2017-03-01 02:03:53 +0100

    exported snackbar-ref

--- src/snackbar/index.ts
+++ src/snackbar/index.ts
@@ -2,3 +2,4 @@ export * from "./snackbar-host.element";
 export * from "./simple-snackbar.element";
 export * from "./snackbar.config";
 export * from "./snackbar.service";
+export * from "./snackbar-ref";
