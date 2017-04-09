498153005c121a25890e54b1cf98df7366af5da5
Stephen Lautier <stephen.lautier@outlook.com>
2017-04-09 04:33:32 +0200

    highlight: typedoc + readme + minor formatting

--- src/highlight/highlight.value-convetor.ts
+++ src/highlight/highlight.value-convetor.ts
@@ -1,5 +1,13 @@
 export class HighlightValueConverter {
 
+	/**
+	 * Converts string with matched `phrase`'s wrapped with span.
+	 *
+	 * @param value text value to evaluate.
+	 * @param phrase text phrase to compare.
+	 * @param [className="highlighted"] cssClass to be added.
+	 * @returns value with matched highlights wrapped with additional class.
+	 */
 	toView(value: string, phrase: string, className = "highlighted") {
 		if (value && phrase) {
 			value = value.replace(new RegExp(`(${phrase})`, "gi"),
